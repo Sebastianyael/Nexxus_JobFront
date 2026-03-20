a67715ea98be7cd028c127ac650bc4e8fe93a383
Sebastianyael <sebastianyael963@gmail.com>
2026-03-19 23:38:40 -0600

    Axios cambiado

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,15 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL : 'http://localhost:8000/api',
+  
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
-})
+});
 
-export default api
+
+api.defaults.withCredentials = true;
+
+export default api;
